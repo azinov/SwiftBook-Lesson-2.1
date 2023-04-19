852f9661f4b9d11f257c3ced27f367d54ac28064
Alexey Zinovyev <zinovkin@gmail.com>
2023-04-19 18:52:24 +0300

    Initial Commit

---
+++ Lesson 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Alexey Zinoviev on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
